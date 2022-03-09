bb197f099b3995b2d584240c717a7492360f927c
Ga <gabri.magni.97@gmail.com>
2022-03-09 15:45:00 +0100

    add function() method (commented)

--- asset/js/main.js
+++ asset/js/main.js
@@ -30,3 +30,37 @@ if (isNaN(kmQuery) || isNaN(ageQuery)) {
 }
 
 
+
+//METHOD WITH FUNCTION()
+
+// let mainContainer = document.getElementById("main-container");
+
+// let kmQuery = prompt("Benvenuto sul sito, inserisca quanti kilometri deve percorrere:");
+// let ageQuery = prompt("Inserisca la sua età in anni:");
+
+// let totPriceRounded;
+// let totPrice
+
+// kmQuery = parseInt(kmQuery);
+// ageQuery = parseInt(ageQuery);
+
+// function price() {
+//     totPrice = kmQuery * 0.21;
+//     if (ageQuery < 18) {
+//         totPrice -= (totPrice * 0.2);
+//     } else if (ageQuery > 64) {
+//         totPrice -= (totPrice * 0.4);
+//     } else {
+//         totPrice = totPrice
+//     }
+//     totPriceRounded = Math.round((totPrice + Number.EPSILON) * 100) / 100;
+//     return totPriceRounded;
+// }
+
+// if (isNaN(kmQuery) || isNaN(ageQuery)) {
+//     mainContainer.innerHTML = `<p>ERRORE, ricaricare la pagina e inserire correttamente i dati</p>`
+// } else {
+//     mainContainer.innerHTML = `<p>Prezzo biglietto: ${price()} &euro;</p>`
+// }
+
+
